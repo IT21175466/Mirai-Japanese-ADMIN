ff27bcfbf9fe87071873681776e1c71a84c23020
Chamath <chamath@flexiata.lk>
2024-05-16 15:39:48 +0530

    OK

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Mirai Admin',
       theme: ThemeData(
         // This is the theme of your application.
         //
